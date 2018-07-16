ed6cc8c36f54cfefa86610adcdf6baab3ef0b4f8
CUH-GWX9 <CUH-GWX9@172.16.111.172>
2018-07-16 15:33:23 +0200

    Dublettenprüfung funktioniert
    Task-Url:

--- cobusJsonProjekt/src/cobusJsonProjekt/parseTheJSON.java
+++ cobusJsonProjekt/src/cobusJsonProjekt/parseTheJSON.java
@@ -1,25 +1,18 @@
 package cobusJsonProjekt;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
 import java.net.URL;
 import java.net.URLConnection;
-import java.nio.charset.Charset;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
-import java.util.Scanner;
-
 import org.apache.commons.io.IOUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -27,64 +20,23 @@ import org.json.JSONObject;
 
 public class parseTheJSON {
 	
-	private static String readAll(Reader rd) throws IOException {
-	    StringBuilder sb = new StringBuilder();
-	    int cp;
-	    while ((cp = rd.read()) != -1) {
-	      sb.append((char) cp);
-	    }
-	    return sb.toString();
-	  }
-	
-	public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
-	    InputStream is = new URL(url).openStream();
-	    try {
-	      BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-	      String jsonText = readAll(rd);
-	      JSONObject json = new JSONObject(jsonText);
-	      return json;
-	    } finally {
-	      is.close();
-	    }
-	  }
-	
-	public static void printJsonObject(JSONObject jsonObj) {
-	    for (Object key : jsonObj.keySet()) {
-	        //based on your key types
-	        String keyStr = (String)key;
-	        Object keyvalue = jsonObj.get(keyStr);
-
-	        //Print key and value
-//	        if(keyStr == "company")
-	        	System.out.println("key: "+ keyStr + " value: " + keyvalue);
-	        
-	        //for nested objects iteration if required
-	        if (keyvalue instanceof JSONObject)
-	            printJsonObject((JSONObject)keyvalue);
-	    }
-	}
-	
-	public static boolean checkDublicates(JSONArray jsonArray, ResultSet resultSet) throws SQLException {
-		boolean justCheckingThingsOut = false;
-		for(int j = 0; j < jsonArray.length(); j++) {
-			JSONObject jObject = jsonArray.getJSONObject(j);
-			String comp = jObject.getString("company");
-			String jComp = comp.toLowerCase();
-			while(resultSet.next()) {
-				String sqlCompanyCheck = resultSet.getString("COMPNAME");
-				String sqlCompanyCheckLower = sqlCompanyCheck.toLowerCase();
-				if(sqlCompanyCheckLower.equals(jComp)) {
-					justCheckingThingsOut = true;
+	public static boolean getMyJSON(JSONObject jsonObject, ResultSet rs) throws SQLException{
+		boolean isTrue = false;
+			String company = jsonObject.getString("company");
+			String email = jsonObject.getString("email");
+			String jsonComp = company.toLowerCase();
+			
+			while(rs.next()) {
+				String sqlCompName = rs.getString("COMPNAME");
+				String sqlComp = sqlCompName.toLowerCase();
+				if(sqlComp.equals(jsonComp)) {
+					isTrue = true;
 					break;
 				}else {
-					justCheckingThingsOut =  false;
+					isTrue = false;
 				}
 			}
-			if(justCheckingThingsOut == true) {
-				System.out.println("die Firma " + jComp + " ist bereits im System hinterlegt");
-			} 
-		}
-		return justCheckingThingsOut;
+		return isTrue;
 	}
 
 	public static void main(String[] args) throws IOException, SQLException {
@@ -123,45 +75,30 @@ public class parseTheJSON {
 		encoding = encoding == null ? "UTF-8" : encoding;
 		String body = IOUtils.toString(in, encoding);	
 		
-		//JSONObject beginnt ab '{' statt bei '['
+		//JSONObject body beginnt ab '{' statt bei '['
 		JSONObject jsonObj = new JSONObject(body.substring(body.indexOf('{')));
 		
 		//Select Statement aus der Config lesen
 		String sqlSelect = props.getProperty("selectString");
         selectStmt = connection.createStatement();
-        resultSet = selectStmt.executeQuery(sqlSelect);
         
         //Select Statement aus der Config für die AddressOrel Tabelle auslesen
         String sqlSelectOrel = props.getProperty("selectStringOrel");
         selectStmtOrel = connection.createStatement();
         resultSetOrel = selectStmtOrel.executeQuery(sqlSelectOrel);
         
-        Boolean isDublicate = false;
-		try {
+        try {
 			JSONArray jsonArray = new JSONArray(body);
 			
 			int count = jsonArray.length(); // get totalCount of all jsonObjects
 			for(int i=0 ; i < count; i++){   // iterate through jsonArray 
-
+				resultSet = selectStmt.executeQuery(sqlSelect);
 				JSONObject jsonObject = jsonArray.getJSONObject(i);  // get jsonObject @ i position 
 				String company = jsonObject.getString("company");
 				String email = jsonObject.getString("email");
-				String jsonComp = company.toLowerCase();
-				boolean tempBoo = checkDublicates(jsonArray, resultSet);
+				boolean tempBoo = getMyJSON(jsonObject, resultSet);
 				
-				while(resultSet.next()) {
-		        	compName = resultSet.getString("COMPNAME");
-		        	String sqlComp = compName.toLowerCase();
-		        	if(sqlComp.equals(jsonComp)) {
-		        		isDublicate = true;
-//		        		System.out.println("Duplikat " + sqlComp + " " + jsonComp);
-		        		break;
-		        	}else {
-		        		isDublicate = false;
-//		        		System.out.println("Kein Duplikat " + sqlComp + " " + jsonComp);
-		        	}
-		        }
-				if(isDublicate) {
+				if(tempBoo == true) {
 	                System.out.println("Die Firma " + company + " gibt es bereits im System");
 				}else {
 					String callProc = "{call dbo.cobus (?, ?)}";
@@ -170,11 +107,11 @@ public class parseTheJSON {
 					stmt.setString(2,  email);
 	                stmt.execute();
 	                
-	                String tableGuid = resultSetOrel.getString("GGUID");
-	                String callProcOrel = "{call dbo.cobusOrel (?)}";
-					stmtOrel = connection.prepareCall(callProcOrel);
-					stmtOrel.setString(1, tableGuid);
-	                stmtOrel.execute();
+//	                String tableGuid = resultSetOrel.getString("GGUID");
+//	                String callProcOrel = "{call dbo.cobusOrel (?)}";
+//					stmtOrel = connection.prepareCall(callProcOrel);
+//					stmtOrel.setString(1, tableGuid);
+//	                stmtOrel.execute();
 				}
 			}
             
